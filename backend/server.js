30f141f69876bc73e18a7b200217751944a33aa0
Yash Bansode <yashbansode1110@gmail.com>
2026-03-05 18:16:52 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://artpixel-gallery.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
